61d3b08149b0009cd6f717b41f4bd03c9c14a3a3
riordansantos <riordan3607@gmail.com>
2021-07-30 15:58:40 -0300

    Novas telas e autenticação do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import {Button} from './components/Button'
+import { BrowserRouter, Route } from "react-router-dom"
+import {NewRoom} from "./pages/NewRoom"
+import {Home} from "./pages/Home"
 
 function App() {
   return (
-    <div>
-      <Button></Button>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom}/>
+
+    </BrowserRouter>
+   
   );
 }
 
